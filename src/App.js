13f36c1067d97308e2fe2260e4e5bb487d25041e
Mystekerinos <jordy.saintalbin@gmail.com>
2022-08-05 02:02:28 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
 import Accueil from "./pages/Accueil";
+// import ItemCity from "./pages/ItemCity";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="">
-      <Accueil />
+    <div>
+      //for routing applicatio
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/Accueil" />} />
+        <Route exact path="/Accueil" element={<Accueil />} />
+        <Route path="*" element={<Navigate to="/" />} />
+        // pass input value from the search bar to another component
+        {/* <Route exact path="/ItemCity/:enteredSearch" element={<ItemMovie/>} />   */}
+      </Routes>
     </div>
   );
 }
