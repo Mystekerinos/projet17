511add4e72c7cd5f45a9cd960094605f093cdda5
Mystekerinos <jordy.saintalbin@gmail.com>
2022-08-17 03:54:23 +0200

    text de la method BEM sur le fichier Accueil.css

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Accueil.css";
 
 const Accueil = () => {
   return (
     <div>
-      <h1>Bonjour</h1>;
+      <section>
+        <h1 class="section__heading">Bonjour</h1>
+
+        <p>
+          Je m'appelle jordy, et je suis en train d'apprendre le HTML et le CSS
+        </p>
+        <p>
+          Ce n'est vraiment pas facile mais j'aime bien, car je trouve cela
+          intéréssant
+        </p>
+      </section>
     </div>
   );
 };
